80aeab151939c0dda0cecdfc90710bee7907f16e
OlSarLang <o.sarholm@hotmail.com>
2020-01-10 13:36:56 +0100

    Add project files.

---
+++ GymBooking/Core/Models/ApplicationUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GymBooking.Core.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+      
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+        public DateTime MemberSince { get; set; }
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        public virtual ICollection<ApplicationUserGymClass> AttendedClasses { get; set; }
+    }
+}
